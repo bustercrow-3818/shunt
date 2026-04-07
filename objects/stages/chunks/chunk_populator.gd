05ffec2d138e7af3d755132a898c1f41cb49ecd4
bustercrow-3818 <bustercrow@live.com>
2026-04-07 13:09:51 -0500

    feat: populator basic function, need stage manager

--- objects/stages/chunks/chunk_populator.gd
+++ objects/stages/chunks/chunk_populator.gd
@@ -2,13 +2,31 @@ extends Node2D
 class_name ChunkPopulator
 
 @export var chunk_pool: ChunkPool
+@export var chunk_size: int = 8
+@export var test_blueprint: Blueprint
+
+func _ready() -> void:
+	%Button.pressed.connect(build_blueprint.bind(test_blueprint))
 
 func build_blueprint(_bp: Blueprint) -> void:
+	print("building %s tiles" % str(_bp._chunk_map.size()))
 	for chunk in _bp._chunk_map:
+		print("processing chunk %s" % str(_bp._chunk_map[chunk]))
+		var _valid_options: Array[ChunkData]
+		var new_chunk_data: ChunkData
+		var new_chunk: TileMapLayer
 		
-		pass
-	pass
+		for i in chunk_pool.chunks:
+			if i.exit_bitmask == _bp._chunk_map[chunk]:
+				_valid_options.append(i)
+			else:
+				pass
+		new_chunk_data = _valid_options.pick_random()
+		new_chunk = new_chunk_data.chunk.instantiate()
+		place_chunk(new_chunk, chunk * new_chunk_data.get_coord_offset())
+		
 
-func place_chunks(_coords: Array[Vector2i], _chunk_size: int) -> void:
-	
-	pass
+func place_chunk(_chunk: TileMapLayer, _coords: Vector2i) -> void:
+	print("placing chunk %s" % str(_chunk))
+	add_child(_chunk)
+	_chunk.global_position = _coords
--- objects/stages/chunks/data/chunk_data.gd
+++ objects/stages/chunks/data/chunk_data.gd
@@ -2,5 +2,10 @@ extends Resource
 class_name ChunkData
 
 @export var chunk: PackedScene
-@export_flags("North:1", "South:2", "East:4", "West:8") var exit_bitmask
+@export_flags("North:1", "South:2", "East:4", "West:8") var exit_bitmask: int
 @export var weight: float = 1.0
+@export var tile_size: int = 32
+@export var chunk_size: int = 16
+
+func get_coord_offset() -> int:
+	return tile_size * chunk_size - tile_size
